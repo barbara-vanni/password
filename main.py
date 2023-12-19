1220998ae19305a8b9f249aa5cc048e52cb0a2ad
Dhjali <barbara.vanni@laplateforme.io>
2023-12-19 10:28:56 +0100

    add random password

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 import hashlib
+import random
 import json
+import string
 
 # def getPassword():
 #     return input("Entrez votre mot de passe : ")
@@ -43,26 +45,45 @@ def load_passwords():
     try:
         with open("passwords.json", "r") as file:
             passwords = json.load(file)
+            passwords_save = []
+            # passwords_save.append(passwords)
     except FileNotFoundError:
         passwords = {}
     return passwords
 
-def add_password():
-    username = input("Entrez le nom d'utilisateur : ")
-    password = input("Entrez le mot de passe : ")
 
-    if valid_password(password) == True:
+
+def check_double(hashed_password, register_password):
+    if hashed_password in register_password:
+        print("Le mot de passe existe déjà !")
+
+def add_password(username, password):
+    register_password = []
+    if valid_password(password):
         hashed_password = hash_password(password)
 
         passwords = load_passwords()
-        passwords[username] = hashed_password
-        save_passwords(passwords)
 
-        print("Mot de passe ajouté avec succès.")
-    else :
+        if check_double(hashed_password,register_password):
+            print("Le mot de passe existe déjà. Veuillez en choisir un autre.")
+        else:
+            passwords[username] = hashed_password
+            save_passwords(passwords)
+            register_password.append(hashed_password)
+            print("Mot de passe ajouté avec succès.")
+            print(register_password)
+    else:
+        print("Mot de passe invalide")
         password = input("Réessayer : ")
-        
+        add_password(username, password)
 
+
+def generer_mot_de_passe(longueur):
+    caracteres = string.ascii_letters + string.digits + string.ascii_uppercase + "[!@#$%^&*]"
+    password = ''.join(random.choice(caracteres) for i in range(longueur))
+    return password
+
+  
 def show_passwords():
     passwords = load_passwords()
     if passwords:
@@ -73,15 +94,24 @@ def show_passwords():
         print("Aucun mot de passe enregistré.")
 
 
+
 def main():
     while True:
-        print("\n1. Ajouter un nouveau mot de passe")
+        print("\n0. Générer mot de passe aléatoirement")
+        print("1. Ajouter un nouveau mot de passe")
         print("2. Afficher les mots de passe")
         print("3. Quitter")
-        choice = input("Choisissez une option (1/2/3) : ")
+        choice = input("Choisissez une option (0/1/2/3) : ")
 
-        if choice == "1":
-            add_password()
+        if choice == "0" : 
+            username = input("Entrez le nom d'utilisateur : ")
+            password = generer_mot_de_passe(12)
+            print(password)
+            add_password(username, password)
+        elif choice == "1":
+            username = input("Entrez le nom d'utilisateur : ")
+            password = input("Entrez le mot de passe : ")
+            add_password(username, password)
         elif choice == "2":
             show_passwords()
         elif choice == "3":
@@ -93,3 +123,5 @@ def main():
 main()
 
  
+
+
